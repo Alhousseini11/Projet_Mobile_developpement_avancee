54151167eada2cebe0652fe2a82e7c9251b81238
unknown <alhousseinimaiga055@gmail.com>
2026-03-18 04:53:17 -0400

    Ignore Windows system files in webpack watcher

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,37 @@
 const webpack = require("@nativescript/webpack");
 
+function toArray(value) {
+	if (!value) {
+		return [];
+	}
+
+	return Array.isArray(value) ? value : [value];
+}
+
+function normalizeIgnoredEntries(entries) {
+	return entries.filter((entry) => typeof entry === "string" && entry.length > 0);
+}
+
 module.exports = (env) => {
 	webpack.init(env);
 
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
 
-	return webpack.resolveConfig();
+	const config = webpack.resolveConfig();
+	const ignored = normalizeIgnoredEntries(toArray(config.watchOptions?.ignored));
+
+	config.watchOptions = {
+		...(config.watchOptions || {}),
+		ignored: [
+			...ignored,
+			"**/pagefile.sys",
+			"**/swapfile.sys",
+			"**/hiberfil.sys",
+			"**/$Recycle.Bin/**",
+			"**/System Volume Information/**",
+		],
+	};
+
+	return config;
 };
